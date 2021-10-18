b8e9c6c2938617dc96b632306e67cd896c75d072
Maria Kuchinskaya <mariakuchinskaya16@gmail.com>
2021-10-19 00:08:51 +0300

    Task1 and Task2 done
    Lesson 3

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            const double pi = 3.141;
+
+            Console.WriteLine("Insert the number:");
+
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            var square = pi * r*(r + i);
+
+            Console.Write($"Your result is: {square}"); 
+
+        }
+    }
+}
+
+
---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int x = 14;
+            int z = 5;
+            int y = 1;
+
+            int result1 = x+= (y - (x++ * z));
+            Console.WriteLine($"You first result is:{result1}");
+
+            int result2 = z = (--x - y * 5);
+            Console.WriteLine($"You second result is:{result2}");
+
+            int result3 = x/= x + (5 % z);
+            Console.WriteLine($"You third result is:{result3}");
+
+            int result4 = z = ( x++ + (y * 5));
+            Console.WriteLine($"You fourth result is:{result4}");
+
+            int result5 = x = y - (x++ * z);
+            Console.WriteLine($"You fifth result is:{result5}");
+
+        }
+    }
+}
